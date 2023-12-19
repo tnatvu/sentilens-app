09b18a41f7b1efe4d20e94a590c8522ef6b07ca6
tnatvu <85584989+tnatvu@users.noreply.github.com>
2023-12-19 16:42:16 +0000

    picture nav

--- utils/page_setup.py
+++ utils/page_setup.py
@@ -1,39 +1,39 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page 
 from st_clickable_images import clickable_images
+import pandas as pd
 
 def click_button(category):
     st.session_state.category = category
 
 def custom_sidebar():
-    categories = ['Laptop', 'outdoor', 'books', 'food']
+    categories = pd.DataFrame({'category': ['Laptop', 'outdoor', 'food', 'books']
+                              ,'url': ["https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=700"
+                                       ,"https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700"
+                                       ,"https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700"
+                                       ,"https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700"
+                                       ]})
+    
     
     with st.sidebar: 
         home_button = st.button('Home')
 
         # Category buttons
-        for x in categories:
-            st.button(x, key=f'sb_{x}', on_click=click_button, args=[x])
+        # for x in categories:
+        #     st.button(x, key=f'sb_{x}', on_click=click_button, args=[x])
+        # products = ['Laptop','food','books', 'outdoor']
 
-
-        clicked = clickable_images(
-            [
-                "https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=700",
-                "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
-                "https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700",
-                "https://images.unsplash.com/photo-1591797442444-039f23ddcc14?w=700",
-                "https://images.unsplash.com/photo-1518727818782-ed5341dbd476?w=700",
-            ],
-            titles=[f"Image #{str(i)}" for i in range(5)],
+        clicked = clickable_images(list(categories['url']),
+            titles=[categories['category'][i] for i in range(len(categories))],
             div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
-            img_style={"margin": "5px", "height": "200px"},
+            img_style={"margin": "5px", "height": "100px", 'width':'100px'},
         )
-        products = ['Laptop','food','books', 'outdoor']
+        
     
         if clicked>-1:
             st.markdown(f"Image #{clicked}")
-            st.markdown(f"Image #{clicked} - {products[clicked]} clicked" if clicked > -1 else "No image clicked")
-            click_button(products[clicked])
+            st.markdown(f"Image #{clicked} - {categories['category'][clicked]} clicked" if clicked > -1 else "No image clicked")
+            click_button(categories['category'][clicked])
     
     if home_button:
         # st.session_state.find_product_reviews = False
@@ -57,10 +57,12 @@ def setup_page(current_page):
     custom_sidebar()
     
     if 'category' not in st.session_state:
-        st.write('hello no cate')
+        # st.write('hello no cate')
         st.session_state.category = False
         
     elif (st.session_state.category != False) & (current_page != 'select item'):
         # st.session_state.category = 'Laptop'
         switch_page('select item')
-
+    
+    elif current_page == 'item':
+        switch_page('item')
