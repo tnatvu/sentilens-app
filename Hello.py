0192b630d82773484db7a8f033fe8aad7470b09d
tnatvu <85584989+tnatvu@users.noreply.github.com>
2023-12-19 19:12:34 +0000

    test

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit_extras
 from streamlit_extras.switch_page_button import switch_page 
-import utils.page_setup as page_setup
+# import utils.page_setup as page_setup
 
 st.markdown('''# Welcome to SentiLens''')
 
@@ -10,6 +10,6 @@ find_product = st.button('Find your product\'s reviews')
 if find_product:
     switch_page('select category')
 
-page_setup.setup_page('home')
+# page_setup.setup_page('home')
 
 
