e3b33d3266bd60d9089fe400490dd181ff5d4a2b
tnatvu <85584989+tnatvu@users.noreply.github.com>
2023-12-19 11:01:02 +0000

    general setup

--- Hello.py
+++ Hello.py
@@ -1,51 +1,21 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
+from streamlit_extras.switch_page_button import switch_page 
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
 
-    st.write("# Welcome to Streamlit! 👋")
+def click_button(cat):
+    st.session_state.clicked = True
+    st.session_state.category = cat
 
-    st.sidebar.success("Select a demo above.")
+st.markdown('# Select product categories:')
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+categories = ['laptop', 'outdoor', 'books', 'food']
 
+for x in categories:
+    st.button(x, on_click=click_button, args=[x])
+# st.button('Food', on_click=click_button, args=['Food'])
 
-if __name__ == "__main__":
-    run()
+if st.session_state.clicked:
+    switch_page('category')
